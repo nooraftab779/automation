ab804733ef2626fd2b51dacdfb1b31669772b875
Noor Aftab <nooraftab779@gmail.com>
2021-12-21 17:31:05 +0500

    jang added

---
+++ jang.py
@@ -0,0 +1,15 @@
+import socket
+import psutil
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname) 
+import platform
+import time
+
+host = '192.168.88.140'
+port = 5001
+s = socket.socket()
+s.connect((host, port))
+while True:
+    a = [platform.node() , platform.processor() , str(round((((psutil.virtual_memory().total)/1024)/1024)/1024)),psutil.cpu_percent()]
+    s.send(bytes(str(a),'utf-8'))
+    time.sleep(10)
